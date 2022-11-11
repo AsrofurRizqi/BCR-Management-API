f2b4531c2627c0b9d8b4b32e3d494635b9527aeb
AsrofurRizqi <asrofurrizqi@gmail.com>
2022-11-11 18:53:40 +0700

    edit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const mainRoutes = require("express").Router();
 const carsRoutes = require("./carsRoutes");
 const authRoutes = require("./authRoutes");
-const openapiRoutes = require("./openapiRoutes");
+const openapiRoutes = require("./openapiroutes");
 
 mainRoutes.use("/cars", carsRoutes);
 mainRoutes.use("/auth", authRoutes);
